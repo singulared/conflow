971df0c4cd10281c1e34c2837955b890b13e384d
Belousow Makc <lib.bmw@gmail.com>
2018-05-16 20:13:50 +0300

    Add initial tests on new Node implementation

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,9 @@ def config_node_data():
             'str': 'string',
             'float': 123.321,
             'list': [1, 2, '3'],
+            'nested_list': [[3, 2, 1], [1, 2, 3]],
+            'nested_map': {'list': [1, 2, 3], 'map': {
+                'str': 'string', 'int': 321}},
             'set': {1, 2, '3'},
             'dict': {'int': 123, 'str': 'other string'},
             'none': None
--- tests/unit/node_test.py
+++ tests/unit/node_test.py
@@ -1,66 +1,85 @@
 import pytest
-from conflow.node import ConfigNode
+from conflow.node import Node, NodeMap, NodeList, TK, TV
 
 
-def test_confignode(config_node_data):
-    node = ConfigNode('test', config_node_data['dict'])
-    assert node.dict
-    assert repr(node) == "ConfigNode('test', {})".format(
-        config_node_data['dict'])
+@pytest.mark.parametrize('value,unvalue,representation', [
+    ('string', 'otherstring', "Node('test', 'string')"),
+    (42, 69, "Node('test', 42)"),
+    (42.3, 69.5, "Node('test', 42.3)"),
+    (True, False, "Node('test', True)"),
+    (None, 1, "Node('test', None)"),
+])
+def test_str(value: TV, unvalue: TV, representation: str) -> None:
+    node = Node('test', value)
+    assert node.value == value
+    assert node.value != unvalue
+    assert node == value
+    assert node != unvalue
+    assert repr(node) == representation
 
 
+'''
+@pytest.mark.skip
 def test_confignode_in(config_node_data):
-    node = ConfigNode('test', config_node_data['dict'])
+    node = Node('test', config_node_data['dict'])
     assert 'int' in node
     assert 'list' in node
     assert 'none' in node
     assert 'missing' not in node
 
 
+@pytest.mark.xfail
 def test_confignode_repr(config_node_data):
-    node = ConfigNode('test', config_node_data)
-    assert repr(node) == 'ConfigNode(\'test\', {})'.format(config_node_data)
+    node = Node('test', config_node_data)
+    assert repr(node) == 'Node(\'test\', {})'.format(config_node_data)
 
 
+@pytest.mark.xfail
 def test_confignode_getitem(config_node_data):
-    node = ConfigNode('test', config_node_data['dict'])
+    node = Node('test', config_node_data['dict'])
     for key, value in (config_node_data['dict'].items()):
-        assert repr(node[key]) == "ConfigNode('{}', {})".format(
+        assert repr(node[key]) == "Node('{}', {})".format(
             key, repr(value))
     with pytest.raises(KeyError):
         node['missing']
 
 
+@pytest.mark.xfail
 def test_confignode_getitem_value(config_node_data):
-    node = ConfigNode('test', config_node_data['dict'])
+    node = Node('test', config_node_data['dict'])
     for key, value in (config_node_data['dict'].items()):
         assert node[key].value == value
 
 
+@pytest.mark.xfail
 def test_confignode_getattr(config_node_data):
-    node = ConfigNode('test', config_node_data['dict'])
+    node = Node('test', config_node_data['dict'])
     for key, value in (config_node_data['dict'].items()):
-        assert repr(getattr(node, key)) == "ConfigNode('{}', {})".format(
+        assert repr(getattr(node, key)) == "Node('{}', {})".format(
             key, repr(value))
-    getattr(node, 'missing') == 'ConfigNode(None, None)'
+    getattr(node, 'missing') == 'Node(None, None)'
 
 
+@pytest.mark.xfail
 def test_confignode_getattr_value(config_node_data):
-    node = ConfigNode('test', config_node_data['dict'])
+    node = Node('test', config_node_data['dict'])
     for key, value in (config_node_data['dict'].items()):
         assert getattr(node, key).value == value
     assert node.missing.value is None
 
 
+@pytest.mark.xfail
 def test_confignode_nested(config_node_data):
-    node = ConfigNode('test', config_node_data)
+    node = Node('test', config_node_data)
     assert node.dict.dict.int.value == 123
     assert node.dict.dict.str.value == 'other string'
     assert node['dict']['dict']['int'].value == 123
     assert node['dict']['dict']['str'].value == 'other string'
 
 
+@pytest.mark.xfail
 def test_confignode_iterator(config_node_data):
-    node = ConfigNode('test', config_node_data['dict'])
+    node = Node('test', config_node_data['dict'])
     assert list(config_node_data['dict'].values()) == [
         subnode.value for subnode in node]
+'''
