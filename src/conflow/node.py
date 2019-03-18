343a1106e2552f0c01489b3f1d1a3947352657ce
Belousow Makc <lib.bmw@gmail.com>
2019-03-18 14:04:57 +0300

    Update node structure

--- src/conflow/node.py
+++ src/conflow/node.py
@@ -1,5 +1,5 @@
-from typing import Sequence, Union, Optional, Iterable, TypeVar, Generic, Text
-from typing import Iterator, Mapping
+from typing import Union, Optional, Iterable, TypeVar, Generic, List
+from typing import Iterator, Mapping, Collection, cast
 
 
 TK = Union[str, int]
@@ -7,24 +7,28 @@ TV = Optional[Union[str, int, float, bool]]
 
 T = TypeVar('T')
 TP = TypeVar('TP')
+TT = TypeVar('TT')
 
 
-def node_factory(key: TK, value: T,
-                 parent: 'Optional[Node[TP]]' = None) -> 'Node':
-    if isinstance(value, Sequence) and not isinstance(value, Text):
-        return NodeList(key, value, parent)
-    elif isinstance(value, dict):
-        return NodeMap(key, value, parent)
-    return Node(key, value, parent)
+class AbstractNode(Generic[T]):
+    @property
+    def value(self) -> Optional[T]: ...
+
+    def compile(self) -> T: ...
 
 
-class Node(Generic[T]):
-    """Base class of Node tree"""
+class Node(AbstractNode[Optional[T]]):
+    """
+    Base class of Node tree
+
+    Implement some type of container for generic value
+    and basic comparsion operation.
+    """
     def __init__(self, key: TK,
                  value: Optional[T] = None,
-                 parent: Optional['Node[TP]'] = None) -> None:
+                 parent: Optional[AbstractNode[TP]] = None):
         """
-        Create instance of ConfigNode.
+        Create instance of Config Node.
 
         :param name: Config record name
         :param value: Config record value
@@ -32,7 +36,7 @@ class Node(Generic[T]):
         """
         self._key: TK = key
         self._value: Optional[T] = value
-        self._parent: Optional[Node[TP]] = parent
+        self._parent: Optional[AbstractNode[TP]] = parent
 
     @property
     def value(self) -> Optional[T]:
@@ -59,35 +63,36 @@ class Node(Generic[T]):
         return self.value != other
 
 
-class NodeList(Node[Iterable[Optional[T]]]):
+class NodeList(AbstractNode[Collection[T]], Collection[AbstractNode[T]]):
     """
-    Class implement Map container for configuration tree node model.
+    Class implement List container for configuration tree node model.
 
     Provide interface for access configuration records.
     """
-    def __init__(self, key: TK, value: Sequence[Optional[T]],
-                 parent: Optional[Node[TP]] = None) -> None:
+    def __init__(self, key: TK, value: Iterable[T],
+                 parent: Optional[AbstractNode[TP]] = None) -> None:
         """
-        Create instance of ConfigList.
+        Create instance of Config List.
 
         :param name: Config record name
-        :param value: Config record values (list)
+        :param value: Config record values (Iterable)
         :param parent: parent Node object
         """
-        super().__init__(key, value, parent)
-        self.__nodes: Sequence[Node[T]] = self.__create_nodes(value)
+        self._key: TK = key
+        self._parent: Optional[AbstractNode[TP]] = parent
+        self.__nodes: List[AbstractNode[T]] = self.__create_nodes(value)
 
     def __repr__(self) -> str:
         """Representation of ConfigList object"""
-        return 'ConfigList({key}, {value})'.format(
+        return 'NodeList({key}, {value})'.format(
             key=repr(self._key), value=repr(self.compile()))
 
-    def compile(self) -> Sequence[Optional[T]]:
+    def compile(self) -> Collection[T]:
         """Method return Node value represented by Python object"""
         return [node.compile() for node in self.__nodes]
 
     def __create_nodes(
-            self, values: Sequence[Optional[T]]) -> Sequence[Node[T]]:
+            self, values: Iterable[T]) -> List[AbstractNode[T]]:
         """
         Create ConfigNodes for all child values
 
@@ -95,7 +100,7 @@ class NodeList(Node[Iterable[Optional[T]]]):
         """
         return [node_factory(self._key, value, self) for value in values]
 
-    def __iter__(self) -> Iterator[Node[T]]:
+    def __iter__(self) -> Iterator[AbstractNode[T]]:
         """
         Implement iterator interface for child nodes
         """
@@ -109,7 +114,7 @@ class NodeList(Node[Iterable[Optional[T]]]):
         """Implementation of != operator"""
         return self.__nodes != other
 
-    def __contains__(self, item: T) -> bool:
+    def __contains__(self, item: object) -> bool:
         """
         Implementation of __contains__ magic method
 
@@ -117,7 +122,13 @@ class NodeList(Node[Iterable[Optional[T]]]):
         """
         return item in self.__nodes
 
-    def __getitem__(self, key: int) -> Node[T]:
+    def __len__(self) -> int:
+        """
+        Implementation of __len__ magic method
+        """
+        return len(self.__nodes)
+
+    def __getitem__(self, key: int) -> AbstractNode[T]:
         """
         Implementation of __getitem__ magic method
         :param key: Access key for data
@@ -125,35 +136,43 @@ class NodeList(Node[Iterable[Optional[T]]]):
         return self.__nodes[key]
 
 
-class NodeMap(Node[Mapping]):
+class NodeMap(AbstractNode[Mapping[TK, T]], Mapping[TK, AbstractNode[T]]):
     """
     Class implement Map container for configuration tree node model.
 
     Provide interface for access configuration records.
     """
     def __init__(self, key: TK, value: Mapping[TK, T],
-                 parent: Optional[Node[TP]] = None) -> None:
+                 parent: Optional[AbstractNode[TP]] = None) -> None:
         """
         Create instance of ConfigNode.
 
         :param name: Config record name
         :param value: Config record value
         """
-        super().__init__(key, value, parent)
-        self.__nodes: Mapping[TK, Node[T]] = self.__create_nodes(value)
+        self._key: TK = key
+        self._parent: Optional[AbstractNode[TP]] = parent
+        self.__nodes: Mapping[TK, AbstractNode[T]] = self.__create_nodes(value)
 
     def __repr__(self) -> str:
         """Representation of ConfigMap object"""
         return 'ConfigMap({key}, {value})'.format(
             key=repr(self._key), value=repr(self._values))
 
-    def __iter__(self) -> Iterable[Node[T]]:
+    def __iter__(self) -> Iterator[TK]:
         """
         Implement iterator interface for child nodes
         """
-        return iter(self.__nodes.values())
+        return iter(self.__nodes)
 
-    def __create_nodes(self, config: Mapping[TK, T]) -> Mapping[TK, Node[T]]:
+    def __len__(self) -> int:
+        """
+        Implementation of __len__ magic method
+        """
+        return len(self.__nodes)
+
+    def __create_nodes(self, config: Mapping[TK, T]
+                       ) -> Mapping[TK, AbstractNode[T]]:
         """
         Create ConfigNodes for all child values
 
@@ -162,25 +181,42 @@ class NodeMap(Node[Mapping]):
         return {key: node_factory(
             key, value, self) for key, value in config.items()}
 
-    def __getattr__(self, name: TK) -> Node[T]:
+    def __getattr__(self, name: TK) -> AbstractNode[T]:
         """
         Implementation of __getattr__ magic method
 
         :param name: Attribute name (data access key)
         """
-        return self.__nodes.get(name, Node(name))
+        return self.__nodes.get(name, cast(AbstractNode[T], Node(name)))
 
-    def __contains__(self, item: TK) -> bool:
+    def __contains__(self, item: TT) -> bool:
         """
         Implementation of __contains__ magic method
 
         :param item: Value for checking on existing in configuration
         """
-        return item in self.__nodes.values()
+        return item in self.__nodes.keys()
 
-    def __getitem__(self, key: TK) -> Node[T]:
+    def __getitem__(self, key: TK) -> AbstractNode[T]:
         """
         Implementation of __getitem__ magic method
         :param key: Access key for data
         """
         return self.__nodes[key]
+
+
+def node_factory(key: TK,
+                 value: TT,
+                 parent: Optional[AbstractNode[TP]] = None,
+                 ) -> AbstractNode[TT]:
+    if isinstance(value, List):
+        return cast(AbstractNode[TT], NodeList(key, value))
+    elif isinstance(value, Mapping):
+        return cast(AbstractNode[TT], NodeMap(key, value))
+    return cast(AbstractNode[TT], Node(key, value))
+
+
+if __name__ == '__main__':
+    node = node_factory('test', [1, 2, 3, 4])
+    print(node.value)
+    len(node)
