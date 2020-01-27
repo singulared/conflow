3114ca4e422e7db2ee104c883687aa342f7aafb7
Andrey Ermilov <andrerm@ya.ru>
2020-01-27 19:26:44 +0300

    issue-107: Refactor from_yaml: change default to allow_exception

--- conflow/froms/yml.py
+++ conflow/froms/yml.py
@@ -5,15 +5,14 @@ TK = Union[str, int]
 T = TypeVar('T')
 
 
-def from_yaml(file_name: str, default: Optional[Dict[TK, T]] = None
-              ) -> Dict[TK, T]:
+def from_yaml(file_name: str, allow_exception: bool = True) -> Dict[TK, T]:
     """Use .yaml file as a source."""
     try:
         with open(file_name, 'r') as file_handler:
             yaml_str  = file_handler.read()
             return yaml.safe_load(yaml_str)
     except FileNotFoundError as error:
-        if default is None:
+        if allow_exception:
             raise FileNotFoundError(error)
         else:
-            return default
+            return {}
